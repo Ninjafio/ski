a62b2f02298afd98991fb92efe6378beb39de1c7
Art Perch <art.perchatkin@gmail.com>
2024-03-04 17:31:45 +0500

    first commit

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Button.css'
+import IProps from '../../types'
+
+const Button = ({children, classname}:IProps) => {
+  return (
+    <button className={ classname }>{ children }</button>
+  )
+}
+
+export default Button
---
+++ src/pages/shopPage/ShopPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { SardiniaBlock, CatalogBlock, GalleryBlock, ExploreBlock, ChooseBlock, VideoBlock, MapBlock } from "../../blocks/index"
+
+const ShopPage = () => {
+  return (
+    <div>
+      <SardiniaBlock />
+      <CatalogBlock />
+      <GalleryBlock />
+      <ExploreBlock />
+      <ChooseBlock />
+      <VideoBlock />
+      <MapBlock />
+    </div>
+  );
+};
+
+export default ShopPage;
